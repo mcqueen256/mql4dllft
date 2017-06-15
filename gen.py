6caa9cadd65f3aba0bca97810789aa8386a514e1
Nicholas Buckeridge <bucknich@gmail.com>
2017-06-15 16:03:16 +1000

    added main file

---
+++ gen.py
@@ -0,0 +1,23 @@
+import click
+
+def download_api():
+  pass
+
+
+def main():
+  """Steps in generating the API:
+  1. Download the MQL4 API
+  2. Decode the Websites into function representation objects (FROs)
+  3. Perform one or more 'optional'
+
+  Optionals:
+    - Generate code for a new Indicator
+    - Generate code for a new Expert Advisor
+    - Generate code for a DLL
+    - Generate all the code
+    - Export generated code
+  """
+  pass
+
+if __name__ == '__main__':
+  main()
