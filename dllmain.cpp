a4450f7f98843669655e19b758f9925274e8e9ac
Nicholas Buckeridge <bucknich@gmail.com>
2017-06-28 14:05:49 +1000

    logging

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,3 +1,8 @@
 #include "export.h"
 void dllmain() {
+	std::string mt4_folder = "C:\\Users\\nicholas\\AppData\\Roaming\\";
+	std::ofstream* fout = new std::ofstream(mt4_folder + "NBRotot_std_log.txt", std::ios::out);
+	std::ofstream* ferr = new std::ofstream(mt4_folder + "NBRotot_err_log.txt", std::ios::out);
+	std::cout.rdbuf(fout->rdbuf());
+	std::cerr.rdbuf(ferr->rdbuf());
 }
