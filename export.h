14d2d5df1100c685e817c4b588cc1439ab42de8d
Nicholas Buckeridge <bucknich@gmail.com>
2017-06-30 10:24:43 +1000

    added init export

--- export.h
+++ export.h
@@ -11,10 +11,33 @@
 #define MT4_API 
 #endif
 
+void dllmain() {
+	std::string mt4_folder = "C:\\Users\\nicholas\\AppData\\Roaming\\MetaQuotes\\Terminal\\3294B546D50FEEDA6BF3CFC7CF858DB7\\";
+	std::ofstream* fout = new std::ofstream(mt4_folder + "MQL4\\Logs\\mql4dllft_std_log.txt", std::ios::out);
+	std::ofstream* ferr = new std::ofstream(mt4_folder + "MQL4\\Logs\\mql4dllft_err_log.txt", std::ios::out);
+	std::cout.rdbuf(fout->rdbuf());
+	std::cerr.rdbuf(ferr->rdbuf());
+	std::cout << "it begins" << std::endl << std::flush;
+}
+
 /* DLL Exported Functions --------------------------------------------------- */
 extern "C" {
 	MT4_API int initialise(int number) {
-		return 0;
+		static bool init = false;
+		if (!init) {
+			dllmain();
+			init = true;
+		}
+		
+		try {
+			NBRobot* robot = new NBRobot(number);
+			Instance::push(robot);
+			return (int)robot;
+		}
+		catch (std::exception& e) {
+			std::cerr << e.what() << std::endl;
+			return 0;
+		}
 	}
 }
 #endif
