82a68fc0f610fd28400acd9d562be0554d39fe28
Nicholas Buckeridge <bucknich@gmail.com>
2017-07-20 13:57:46 +1000

    generate more gcov reports

--- generate_gcov_reports.sh
+++ generate_gcov_reports.sh
@@ -2,10 +2,23 @@
 for file in src/*.cpp
 do
 	f_base=$(basename $file)
-	echo "f_base: $f_base"
 	f_name=${f_base%.*}
-	echo "f_name: $f_name"
 	f="$f_name.test.cpp"
-	echo "f: $f"
+	gcov -cbf $f
+done
+
+for file in include/*.hpp
+do
+	f_base=$(basename $file)
+	f_name=${f_base%.*}
+	f="$f_name.test.hpp"
+	gcov -cbf $f
+done
+
+for file in test/src/*.cpp
+do
+	f_base=$(basename $file)
+	f_name=${f_base%.*}
+	f="$f_name.test.cpp"
 	gcov -cbf $f
 done
