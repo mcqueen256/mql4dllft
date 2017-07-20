f4612448aefd7951ba95236c3833727bc03d1e9e
Nicholas Buckeridge <bucknich@gmail.com>
2017-07-21 08:59:52 +1000

    Added base robot description

--- src/BaseRobot.cpp
+++ src/BaseRobot.cpp
@@ -1,4 +1,12 @@
 #include "BaseRobot.hpp"
+
+/*
+ * The base robot 
+ * The BaseRobot is an abstract class that requires some core methods to be
+ * overriden (onQuote, onBar, onAnalysis) so that the robot can run correctly.
+ * This class also stores some private objects like the buffers and MQL API. 
+ */
+
 /**
  *
  */
